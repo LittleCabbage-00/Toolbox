f21c17e647e2f0b20b664b6aec02a5d590ac8a1a
LittleCabbage-00 <2485535417@qq.com>
2023-02-05 22:49:01 +0800

    update ui and set MainActivity to Bing page

---
+++ app/src/main/java/com/example/cryptoapp/Activities/MainActivity.java
@@ -0,0 +1,483 @@
+package com.example.cryptoapp.Activities;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.cryptoapp.Base.BaseActivity;
+import com.example.cryptoapp.R;
+import com.google.android.material.navigation.NavigationView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener{
+
+    private WebView webView;
+    private ProgressBar progressBar;
+    private EditText textUrl;
+    private ImageView webIcon;
+    private ImageView btnStart;
+    private long exitTime = 0;
+    private Context mContext;
+    private InputMethodManager manager;
+    private static final String HTTP = "http://";
+    private static final String HTTPS = "https://";
+    private static final int PRESS_BACK_EXIT_GAP = 2000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //初始化控件
+        DrawerLayout mDrawerLayout=(DrawerLayout) findViewById(R.id.drawerLayout);
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        NavigationView navView=(NavigationView) findViewById(R.id.navView);
+
+        //顶部设置为toolbar界面
+        setSupportActionBar(toolbar);
+        ActionBar actionBar=getSupportActionBar();
+        //侧滑栏相关设置
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        //侧滑栏选项点击事件
+        navView.setCheckedItem(R.id.home);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    //关于->弹出关于app的窗口
+                    case R.id.info:
+                        AlertDialog.Builder dialog=new AlertDialog.Builder(MainActivity.this);
+                        dialog.setTitle("关于");
+                        dialog.setMessage("作者：LittleCabbage\n" +
+                                "QQ号：2485535417\n"+
+                                "本软件仅做为交流学习用，严禁用于其他用途");
+                        dialog.setCancelable(true);
+                        dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                        dialog.show();
+                        mDrawerLayout.close();
+                        break;
+                    //文本加密->进入相关activity
+                    case R.id.navTextLayout:
+                        Intent intent_text=new Intent(MainActivity.this,TextActivity.class);
+                        startActivity(intent_text);
+                        mDrawerLayout.close();
+                        break;
+                    //文件加密->进入相关activity
+                    case R.id.navFileLayout:
+                        Intent intent_file=new Intent(MainActivity.this,FileActivity.class);
+                        startActivity(intent_file);
+                        mDrawerLayout.close();
+                        break;
+                    //图片解密预览->进入相关activity
+                    case R.id.navPicLayout:
+                        Intent intent_pic=new Intent(MainActivity.this,PicActivity.class);
+                        startActivity(intent_pic);
+                        mDrawerLayout.close();
+                        break;
+                    //主界面->什么也不做
+                    default:
+                        mDrawerLayout.close();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        //web页面初始化
+
+        // 防止底部按钮上移
+        getWindow().setSoftInputMode
+                (WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN |
+                        WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        mContext = MainActivity.this;
+        manager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        // 绑定控件
+        initView();
+        // 初始化 WebView
+        initWeb();
+    }
+
+    //绑定控件
+    private void initView() {
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+        textUrl = findViewById(R.id.textUrl);
+        webIcon = findViewById(R.id.webIcon);
+        btnStart = findViewById(R.id.btnStart);
+        ImageView goBack = findViewById(R.id.goBack);
+        ImageView goForward = findViewById(R.id.goForward);
+        ImageView navSet = findViewById(R.id.navSet);
+        ImageView goHome = findViewById(R.id.goHome);
+        // 绑定按钮点击事件
+        btnStart.setOnClickListener(this);
+        goBack.setOnClickListener(this);
+        goForward.setOnClickListener(this);
+        navSet.setOnClickListener(this);
+        goHome.setOnClickListener(this);
+
+        // 地址输入栏获取与失去焦点处理
+        textUrl.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    // 显示当前网址链接
+                    textUrl.setText(webView.getUrl());
+                    // 光标置于末尾
+                    textUrl.setSelection(textUrl.getText().length());
+                    // 显示因特网图标
+                    webIcon.setImageResource(R.drawable.internet);
+                    // 显示跳转按钮
+                    btnStart.setImageResource(R.drawable.go);
+                } else {
+                    // 显示网站名
+                    textUrl.setText(webView.getTitle());
+                    // 显示网站图标
+                    webIcon.setImageBitmap(webView.getFavicon());
+                    // 显示刷新按钮
+                    btnStart.setImageResource(R.drawable.refresh);
+                }
+            }
+        });
+        // 监听键盘回车搜索
+        textUrl.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    // 执行搜索
+                    btnStart.callOnClick();
+                    textUrl.clearFocus();
+                }
+                return false;
+            }
+        });
+    }
+
+
+    //初始化 web
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initWeb() {
+        // 重写 WebViewClient
+        webView.setWebViewClient(new MkWebViewClient());
+        // 重写 WebChromeClient
+        webView.setWebChromeClient(new MkWebChromeClient());
+
+        WebSettings settings = webView.getSettings();
+        // 启用 js 功能
+        settings.setJavaScriptEnabled(true);
+        // 设置浏览器 UserAgent
+        settings.setUserAgentString(settings.getUserAgentString() + " mkBrowser/" + getVerName(mContext));
+
+        // 将图片调整到适合 WebView 的大小
+        settings.setUseWideViewPort(true);
+        // 缩放至屏幕的大小
+        settings.setLoadWithOverviewMode(true);
+
+        // 支持缩放，默认为true。是下面那个的前提。
+        settings.setSupportZoom(true);
+        // 设置内置的缩放控件。若为false，则该 WebView 不可缩放
+        settings.setBuiltInZoomControls(true);
+        // 隐藏原生的缩放控件
+        settings.setDisplayZoomControls(false);
+
+        // 缓存
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        // 设置可以访问文件
+        settings.setAllowFileAccess(true);
+        // 支持通过JS打开新窗口
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        // 支持自动加载图片
+        settings.setLoadsImagesAutomatically(true);
+        // 设置默认编码格式
+        settings.setDefaultTextEncodingName("utf-8");
+        // 本地存储
+        settings.setDomStorageEnabled(true);
+        settings.setPluginState(WebSettings.PluginState.ON);
+
+        // 资源混合模式
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        // 加载首页
+        webView.loadUrl(getResources().getString(R.string.home_url));
+    }
+
+
+    /**
+     * 重写 WebViewClient
+     */
+    private class MkWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // 设置在webView点击打开的新网页在当前界面显示,而不跳转到新的浏览器中
+
+            if (url == null) {
+                // 返回true自己处理，返回false不处理
+                return true;
+            }
+
+            // 正常的内容，打开
+            if (url.startsWith(HTTP) || url.startsWith(HTTPS)) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            // 调用第三方应用，防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
+            try {
+                // TODO:弹窗提示用户，允许后再调用
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            } catch (Exception e) {
+                return true;
+            }
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            // 网页开始加载，显示进度条
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.VISIBLE);
+
+            // 更新状态文字
+            textUrl.setText("加载中...");
+
+            // 切换默认网页图标
+            webIcon.setImageResource(R.drawable.internet);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            // 网页加载完毕，隐藏进度条
+            progressBar.setVisibility(View.INVISIBLE);
+
+            // 改变标题
+            setTitle(webView.getTitle());
+            // 显示页面标题
+            textUrl.setText(webView.getTitle());
+        }
+    }
+
+
+    /**
+     * 重写 WebChromeClient
+     */
+    private class MkWebChromeClient extends WebChromeClient {
+        private final static int WEB_PROGRESS_MAX = 100;
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            // 加载进度变动，刷新进度条
+            progressBar.setProgress(newProgress);
+            if (newProgress > 0) {
+                if (newProgress == WEB_PROGRESS_MAX) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+            // 改变图标
+            webIcon.setImageBitmap(icon);
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+
+            // 改变标题
+            setTitle(title);
+            // 显示页面标题
+            textUrl.setText(title);
+        }
+    }
+
+    /**
+     * 返回按钮处理
+     */
+    @Override
+    public void onBackPressed() {
+        // 能够返回则返回上一页
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            if ((System.currentTimeMillis() - exitTime) > PRESS_BACK_EXIT_GAP) {
+                // 连点两次退出程序
+                Toast.makeText(mContext, "再按一次退出程序",
+                        Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                super.onBackPressed();
+            }
+
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // 跳转 或 刷新
+            case R.id.btnStart:
+                if (textUrl.hasFocus()) {
+                    // 隐藏软键盘
+                    if (manager.isActive()) {
+                        manager.hideSoftInputFromWindow(textUrl.getApplicationWindowToken(), 0);
+                    }
+                    // 地址栏有焦点，是跳转
+                    String input = textUrl.getText().toString();
+                    if (!isHttpUrl(input)) {
+                        // 不是网址，加载搜索引擎处理
+                        try {
+                            // URL 编码
+                            input = URLEncoder.encode(input, "utf-8");
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        input = "https://cn.bing.com/search?q=" + input ;
+                    }
+                    webView.loadUrl(input);
+                    // 取消掉地址栏的焦点
+                    textUrl.clearFocus();
+                } else {
+                    // 地址栏没焦点，是刷新
+                    webView.reload();
+                }
+                break;
+            // 后退
+            case R.id.goBack:
+                webView.goBack();
+                break;
+            // 前进
+            case R.id.goForward:
+                webView.goForward();
+                break;
+            // 设置
+            case R.id.navSet:
+                Toast.makeText(mContext, "功能开发中", Toast.LENGTH_SHORT).show();
+                break;
+
+            // 主页
+            case R.id.goHome:
+                webView.loadUrl(getResources().getString(R.string.home_url));
+                break;
+
+            default:
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            webView.getClass().getMethod("onPause").invoke(webView);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            webView.getClass().getMethod("onResume").invoke(webView);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        NavigationView navView=(NavigationView) findViewById(R.id.navView);
+        navView.setCheckedItem(R.id.home);
+        setTitle("主界面");
+    }
+
+    //toolbar上的菜单逻辑
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        TextView sysInfo_dis=(TextView) findViewById(R.id.sysInfo);
+        //使用menu.layout作为界面
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        //初始化侧滑栏中系统信息的字符串
+        String sysInfo=String.format(getResources().getString(R.string.sysInfo), Build.VERSION.RELEASE,Build.VERSION.SDK);
+        sysInfo_dis.setText(sysInfo);
+        return true;
+    }
+
+    public static boolean isHttpUrl(String urls) {
+        boolean isUrl;
+        // 判断是否是网址的正则表达式
+        String regex = "(((https|http)?://)?([a-z0-9]+[.])|(www.))"
+                + "\\w+[.|\\/]([a-z0-9]{0,})?[[.]([a-z0-9]{0,})]+((/[\\S&&[^,;\u4E00-\u9FA5]]+)+)?([.][a-z0-9]{0,}+|/?)";
+
+        Pattern pat = Pattern.compile(regex.trim());
+        Matcher mat = pat.matcher(urls.trim());
+        isUrl = mat.matches();
+        return isUrl;
+    }
+
+    /**
+     * 获取版本号名称
+     *
+     * @param context 上下文
+     * @return 当前版本名称
+     */
+    private static String getVerName(Context context) {
+        String verName = "unKnow";
+        try {
+            verName = context.getPackageManager().
+                    getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return verName;
+    }
+
+}
