12f89c8530df42993e613d7989150f9c1cff0e45
LittleCabbage-00 <2485535417@qq.com>
2023-03-27 20:47:41 +0800

    change target from 33 to 29 to fix bug and update README

--- app/src/main/java/com/example/cryptoapp/Base/BaseActivity.kt
+++ app/src/main/java/com/example/cryptoapp/Base/BaseActivity.kt
@@ -57,25 +57,4 @@ open class BaseActivity:AppCompatActivity(){
         }
         return true
     }
-
-    //根据包名 判断某APP是否安装
-    fun checkApkExist(context: Context,packageName: String) :Boolean{
-        //  检查app是否有安装
-        if (TextUtils.isEmpty(packageName))
-            return  false
-        try {
-            val info = context.packageManager
-                .getApplicationInfo(
-                    packageName,
-                    PackageManager.GET_UNINSTALLED_PACKAGES
-                )
-            // Timber.d(info.toString()) // Timber 是我打印 log 用的工具，这里只是打印一下 log
-            Log.d("Base:","yes")
-            return true
-        } catch (e: PackageManager.NameNotFoundException) {
-            // Timber.d(e.toString()) // Timber 是我打印 log 用的工具，这里只是打印一下 log
-            Log.d("Base:","No")
-            return false
-        }
-    }
 }
